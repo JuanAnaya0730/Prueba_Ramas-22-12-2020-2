9e62f23ea2d79e809c83b99a9ad5aa50f03c7917
Juan Anaya <juan.anayar@udea.edu.co>
2020-12-22 20:48:30 -0500

    Inicializacion variable que almacena cadena de entrada y imprecion cararcter a caracter.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <iostream>
+
+using namespace std;
+
+#define TAM (int)10
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    char A[TAM] = {"AB11CD44"};
+
+    for(int var=0; var<TAM; ++var){
+        cout << A[var] << endl;
+    }
+
+    //MainWindow w;
+    //w.show();
     return a.exec();
 }
